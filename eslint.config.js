798de3f9b8a1576b91303aa0d2b05d9fa3a71662
junjuny0227 <junjuny0227@gmail.com>
2025-10-02 09:55:15 +0900

    chore: lint 스크립트 개선 및 적용 범위 확장

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ import tseslint from 'typescript-eslint';
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
