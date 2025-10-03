648daea559b60cea7c3b0865c3d126e9746a790e
junjuny0227 <junjuny0227@gmail.com>
2025-10-03 23:19:37 +0900

    feat: 스텝에 따른 페이지 렌더링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
+import {
+  AiConversionPage,
+  CameraPage,
+  InfoInputPage,
+  StartPage,
+  ThemeSelectPage,
+} from './pageContainer';
+import { STEP, type Step } from './types';
+
 const App = () => {
-  return <>who are you v2</>;
+  const [step, setStep] = useState<Step>(STEP.START);
+
+  return (
+    <div className="h-screen bg-[#f8f8f8]">
+      {step === STEP.START && <StartPage />}
+      {step === STEP.CAMERA && <CameraPage />}
+      {step === STEP.AI_CONVERSION && <AiConversionPage />}
+      {step === STEP.INFO_INPUT && <InfoInputPage />}
+      {step === STEP.THEME_SELECT && <ThemeSelectPage />}
+    </div>
+  );
 };
 
 export default App;
