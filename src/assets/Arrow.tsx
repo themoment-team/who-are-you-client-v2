8a3012bdeedb075138dbd96750531ebf57008680
junjuny0227 <junjuny0227@gmail.com>
2025-10-07 00:52:32 +0900

    refactor: Arrow 컴포넌트 코드 개선

--- src/assets/Arrow.tsx
+++ src/assets/Arrow.tsx
@@ -9,15 +9,18 @@ const Arrow = ({ flip = false }: ArrowProps) => (
     height="36"
     viewBox="0 0 36 36"
     fill="none"
-    style={flip ? { transform: 'rotate(180deg)', cursor: 'pointer' } : { cursor: 'pointer' }}
+    style={{
+      transform: flip ? 'rotate(180deg)' : undefined,
+      cursor: 'pointer',
+    }}
   >
     <path
       d="M23.625 6.75L12.375 18L23.625 29.25"
       stroke="#222222"
-      style={{ stroke: '#222222', strokeOpacity: 1 }}
-      strokeWidth="3"
+      strokeWidth={3}
       strokeLinecap="round"
       strokeLinejoin="round"
+      strokeOpacity={1}
     />
   </svg>
 );
