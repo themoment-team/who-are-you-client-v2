301c02c0ea1cf3d5eec9ba3d5387a271aa50dcee
junjuny0227 <junjuny0227@gmail.com>
2025-10-06 21:55:28 +0900

    feat: Arrow 아이콘 컴포넌트 추가

---
+++ src/assets/Arrow.tsx
@@ -0,0 +1,25 @@
+interface ArrowProps {
+  flip?: boolean;
+}
+
+const Arrow = ({ flip = false }: ArrowProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+    fill="none"
+    style={flip ? { transform: 'rotate(180deg)', cursor: 'pointer' } : { cursor: 'pointer' }}
+  >
+    <path
+      d="M23.625 6.75L12.375 18L23.625 29.25"
+      stroke="#222222"
+      style={{ stroke: '#222222', strokeOpacity: 1 }}
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Arrow;
