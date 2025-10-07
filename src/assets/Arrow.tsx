12a694c08f6cb8240898d18360a5eeb05c3382dc
junjuny0227 <junjuny0227@gmail.com>
2025-10-07 16:53:30 +0900

    refactor: width, height prop 타입을 올바르게 수정

--- src/assets/Arrow.tsx
+++ src/assets/Arrow.tsx
@@ -5,8 +5,8 @@ interface ArrowProps {
 const Arrow = ({ flip = false }: ArrowProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="36"
-    height="36"
+    width={36}
+    height={36}
     viewBox="0 0 36 36"
     fill="none"
     style={{
