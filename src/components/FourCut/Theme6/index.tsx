44c5a9fd4cf9e558a518766cb382c53c5529fe5a
junjuny0227 <junjuny0227@gmail.com>
2025-10-08 00:26:58 +0900

    feat: 인생네컷 테마 컴포넌트 초기 설정

---
+++ src/components/FourCut/Theme6/index.tsx
@@ -0,0 +1,7 @@
+const FourCutTheme6 = () => {
+  return (
+    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 px-[1rem] pt-[1.5rem] pb-[1.6875rem]"></div>
+  );
+};
+
+export default FourCutTheme6;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './BusinessCard';
+export * from './FourCut';
