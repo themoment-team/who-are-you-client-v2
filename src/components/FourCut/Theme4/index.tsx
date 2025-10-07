48126147752b6f09e38197f5cb4233e4fc0c24f6
junjuny0227 <junjuny0227@gmail.com>
2025-10-08 01:06:06 +0900

    style: 인생네컷 테마에 그림자 스타일 추가

--- src/components/FourCut/Theme4/index.tsx
+++ src/components/FourCut/Theme4/index.tsx
@@ -1,6 +1,6 @@
 const FourCutTheme4 = () => {
   return (
-    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 px-[1rem] pt-[1.5rem] pb-[1.6875rem]"></div>
+    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 bg-white px-[1rem] pt-[1.5rem] pb-[1.6875rem] shadow-[0_2px_20px_0_rgba(177,177,177,0.25)]"></div>
   );
 };
 
