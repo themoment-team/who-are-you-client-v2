bc5892bf09771844c7b3ab29e9d679c990ca10c4
junjuny0227 <junjuny0227@gmail.com>
2025-10-08 21:22:47 +0900

    feat: 인생네컷 모든 테마에 GSM 로고 추가

--- src/components/FourCut/Theme4/index.tsx
+++ src/components/FourCut/Theme4/index.tsx
@@ -1,6 +1,10 @@
+import { GSMLogoWithText } from '../../../assets';
+
 const FourCutTheme4 = () => {
   return (
-    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 bg-[#FBD5DC] px-[1rem] pt-[1.5rem] pb-[1.6875rem] shadow-[0_2px_20px_0_rgba(177,177,177,0.25)]"></div>
+    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 bg-[#FBD5DC] px-[1rem] pt-[1.5rem] pb-[1.6875rem] shadow-[0_2px_20px_0_rgba(177,177,177,0.25)]">
+      <GSMLogoWithText color="#FEE3E8" />
+    </div>
   );
 };
 
