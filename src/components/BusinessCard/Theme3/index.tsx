aa5b5cbb1c2dcf5557173d04ab18fccaed869d5b
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 16:22:48 +0900

    feat: BusinessCard Theme3 컴포넌트 구현

--- src/components/BusinessCard/Theme3/index.tsx
+++ src/components/BusinessCard/Theme3/index.tsx
@@ -1,6 +1,35 @@
+import { GSMLogo, ShortBar } from '../../../assets';
+
 const BusinessCardTheme3 = () => {
   return (
-    <div className="h-[21.25rem] w-[11.8125rem] rounded-lg bg-white shadow-[0_2px_6px_0_rgba(209,209,209,0.25)]"></div>
+    <div className="relative flex h-[21.25rem] w-[11.8125rem] flex-col gap-[2.25rem] rounded-lg bg-white p-[1.75rem] shadow-[0_2px_6px_0_rgba(209,209,209,0.25)]">
+      <GSMLogo top={264} left={24} />
+      <img
+        src="/public/images/example.jpg"
+        alt="image"
+        className="h-[8.3125rem] w-[8.3125rem] object-cover object-center"
+      />
+      <div className="flex h-full flex-col justify-between">
+        <div className="flex flex-col gap-[.1875rem]">
+          <p className="text-[1rem] font-semibold tracking-[.04rem]">홍길동</p>
+          <p className="h-[.75rem] text-[.625rem] leading-[.625rem] font-normal tracking-[.0125rem] text-[#888]">
+            UI/UX Designer
+          </p>
+          <ShortBar />
+        </div>
+        <div className="flex flex-col gap-[.3125rem]">
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Major) UI/UX Designer
+          </p>
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Email) honggildong@gmail.com
+          </p>
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Tel) 010-1234-5678
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
