1f5a5a0db05dbd3f1293711c4cc3762fdd9c9876
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 13:57:23 +0900

    feat: ShortBar 아이콘 컴포넌트 추가

---
+++ src/assets/ShortBar.tsx
@@ -0,0 +1,9 @@
+const ShortBar = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={26} height={3} viewBox="0 0 26 3" fill="none">
+    <rect y={3} width={3} height={9} transform="rotate(-90 0 3)" fill="#DADE00" />
+    <rect x={9} y={3} width={3} height={9} transform="rotate(-90 9 3)" fill="#7BD8F7" />
+    <rect x={17} y={3} width={3} height={9} transform="rotate(-90 17 3)" fill="#5385D6" />
+  </svg>
+);
+
+export default ShortBar;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as Arrow } from './Arrow';
 export { default as Dot } from './Dot';
 export { default as GSMLogoWithText } from './GSMLogoWithText';
 export { default as GSMLogo } from './GSMLogo';
+export { default as ShortBar } from './ShortBar';
