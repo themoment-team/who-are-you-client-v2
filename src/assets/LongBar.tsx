db86cbc5e8cf2fab17abf7c6740eee1486b95e99
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 20:38:47 +0900

    style: 불필요한 주석 제거

--- src/assets/LongBar.tsx
+++ src/assets/LongBar.tsx
@@ -1,5 +1,4 @@
 const LongBar = () => (
-  // increased each segment by 1, and the middle segment by +1 extra (total +2)
   <svg xmlns="http://www.w3.org/2000/svg" width={2} height={133} viewBox="0 0 2 133" fill="none">
     <rect width={2} height={44} fill="#DADE00" fillOpacity={1} />
     <rect y={44} width={2} height={45} fill="#7BD8F7" fillOpacity={1} />
