072cda83933592f514229027fcf8b8990f35ad69
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 14:03:32 +0900

    feat: BusinessCard Theme1 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { type CardType, STEP, type Step } from './types';
 
 const App = () => {
   const [step, setStep] = useState<Step>(STEP.START);
-  const [cardType, setCardType] = useState<CardType | undefined>('FOUR_CUT');
+  const [cardType, setCardType] = useState<CardType | undefined>('BUSINESS_CARD');
 
   return (
     <div className="flex h-screen items-center justify-center bg-[#f8f8f8]">
--- src/components/BusinessCard/Theme1/index.tsx
+++ src/components/BusinessCard/Theme1/index.tsx
@@ -1,6 +1,35 @@
+import { GSMLogo, ShortBar } from '../../../assets';
+
 const BusinessCardTheme1 = () => {
   return (
-    <div className="h-[11.8125rem] w-[21.25rem] rounded-lg bg-white shadow-[0_2px_6px_0_rgba(209,209,209,0.25)]"></div>
+    <div className="relative flex h-[11.8125rem] w-[21.25rem] justify-between rounded-lg bg-white p-[1.75rem] shadow-[0_2px_6px_0_rgba(209,209,209,0.25)]">
+      <GSMLogo top={16.5} left={17} />
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-[.1875rem]">
+          <p className="text-[1rem] font-semibold tracking-[.04rem]">홍길동</p>
+          <p className="h-[.75rem] text-[.625rem] leading-[.625rem] font-normal tracking-[.0125rem] text-[#888]">
+            UI/UX Designer
+          </p>
+          <ShortBar />
+        </div>
+        <div className="flex flex-col gap-[.3125rem]">
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Major) UI/UX Designer
+          </p>
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Email) honggildong@gmail.com
+          </p>
+          <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
+            Tel) 010-1234-5678
+          </p>
+        </div>
+      </div>
+      <img
+        src="/public/images/example.jpg"
+        alt="image"
+        className="h-[8.3125rem] w-[8.3125rem] object-cover object-center"
+      />
+    </div>
   );
 };
 
