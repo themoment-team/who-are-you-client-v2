769b3e29a3d4be8cce48a56bf2e8612b8beebaab
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 23:50:36 +0900

    feat: 명함 컴포넌트 데이터 바인딩

--- src/components/BusinessCard/Theme3/index.tsx
+++ src/components/BusinessCard/Theme3/index.tsx
@@ -1,31 +1,32 @@
 import { GSMLogo, ShortBar } from '../../../assets';
+import type { BusinessCardProps } from '../../../types';
 
-const BusinessCardTheme3 = () => {
+const BusinessCardTheme3 = ({ name, major, email, tel, imageSrc }: BusinessCardProps) => {
   return (
     <div className="relative flex h-[21.25rem] w-[11.8125rem] flex-col gap-[2.25rem] bg-white p-[1.75rem] shadow-[0_2px_6px_0_rgba(209,209,209,0.25)] print:border print:border-[#CFCFCF] print:shadow-none">
       <GSMLogo top={264} left={24} />
       <img
-        src="/public/images/example.jpg"
+        src={imageSrc}
         alt="image"
         className="h-[8.3125rem] w-[8.3125rem] rounded-[.2875rem] object-cover object-center"
       />
       <div className="z-10 flex h-full flex-col justify-between">
         <div className="flex flex-col gap-[.1875rem]">
-          <p className="text-[1rem] font-semibold tracking-[.04rem]">홍길동</p>
+          <p className="text-[1rem] font-semibold tracking-[.04rem]">{name}</p>
           <p className="h-[.75rem] text-[.625rem] leading-[.625rem] font-normal tracking-[.0125rem] text-[#888]">
-            UI/UX Designer
+            {major}
           </p>
           <ShortBar />
         </div>
         <div className="flex flex-col gap-[.3125rem]">
           <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
-            Major) UI/UX Designer
+            Major) {major}
           </p>
           <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
-            Email) honggildong@gmail.com
+            Email) {email}
           </p>
           <p className="text-[.5313rem]/[.5906rem] font-normal tracking-[.0106rem] text-[#494949]">
-            Tel) 010-1234-5678
+            Tel) {tel}
           </p>
         </div>
       </div>
