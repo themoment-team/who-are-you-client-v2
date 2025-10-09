29a027e10a7fd7df4440a01c323279571058ac9e
junjuny0227 <junjuny0227@gmail.com>
2025-10-09 23:48:11 +0900

    feat: 명함, 인생네컷 Props 타입 추가

--- src/types/card.ts
+++ src/types/card.ts
@@ -1,1 +1,14 @@
 export type CardType = 'BUSINESS_CARD' | 'FOUR_CUT';
+
+export interface BusinessCardProps {
+  name: string;
+  role: string;
+  major: string;
+  email: string;
+  tel: string;
+  imageSrc: string;
+}
+
+export interface FourCutProps {
+  imageSrcs: string[];
+}
