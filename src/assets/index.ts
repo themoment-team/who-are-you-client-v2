3d196cec218d41e6783b0dbdce0148744f6fd525
junjuny0227 <junjuny0227@gmail.com>
2025-10-10 01:39:13 +0900

    feat: Plus 아이콘 컴포넌트 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as GSMLogoWithText } from './GSMLogoWithText';
 export { default as GSMLogo } from './GSMLogo';
 export { default as ShortBar } from './ShortBar';
 export { default as LongBar } from './LongBar';
+export { default as PlusIcon } from './PlusIcon';
