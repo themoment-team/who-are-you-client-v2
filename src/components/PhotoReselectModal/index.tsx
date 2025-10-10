0c3cd6307d9ea6c797d8a82b335c149949e716d8
junjuny0227 <junjuny0227@gmail.com>
2025-10-10 18:18:59 +0900

    refactor: 가독성 및 유지보수성 향상을 위한 리팩토링

--- src/components/PhotoReselectModal/index.tsx
+++ src/components/PhotoReselectModal/index.tsx
@@ -7,16 +7,22 @@ interface PhotoReselectModalProps {
 }
 
 const PhotoReselectModal = ({ cardType, onClose }: PhotoReselectModalProps) => {
+  const isBusinessCard = cardType === 'BUSINESS_CARD';
+  const cardTypeLabel = isBusinessCard ? '명함' : '인생네컷';
+
+  const IMAGE_CLASS = isBusinessCard ? 'h-[8rem] w-[8rem]' : 'h-[12.8125rem] w-[10.25rem]';
+  const BUTTON_SIZE_CLASS = IMAGE_CLASS;
+  const COMMON_BUTTON_CLASSES = `flex cursor-pointer flex-col items-center justify-center gap-4 rounded-sm bg-[#F8F8F8] ${BUTTON_SIZE_CLASS}`;
+
   return (
     <div className="absolute top-0 left-0 z-20 flex h-[61.5rem] w-[50rem] items-end rounded-[1.5rem] bg-black/20">
       <div className="flex w-full flex-col gap-9 rounded-[1.5rem] bg-white p-12 pb-9">
         <div className="flex flex-col gap-4">
           <h1 className="text-[2.25rem]/[2.25rem] font-black text-[#222]">
-            {cardType === 'BUSINESS_CARD' ? '명함' : ''} 이미지 재선택
+            {cardTypeLabel} 이미지 재선택
           </h1>
           <p className="text-[1.25rem]/[1.875rem] font-medium text-[#666]">
-            {cardType === 'BUSINESS_CARD' ? '명함' : '인생네컷'}에 사용될 이미지를 재선택할 수
-            있습니다. 마음에 드는 이미지가
+            {cardTypeLabel}에 사용될 이미지를 재선택할 수 있습니다. 마음에 드는 이미지가
             <br />
             없을 경우 이미지 재변환으로 새로운 이미지를 생성할 수 있습니다.
           </p>
@@ -25,23 +31,13 @@ const PhotoReselectModal = ({ cardType, onClose }: PhotoReselectModalProps) => {
           <img
             src="/images/example.jpg"
             alt="example"
-            className={`cursor-pointer rounded-sm object-cover object-center ${
-              cardType === 'BUSINESS_CARD' ? 'h-[8rem] w-[8rem]' : 'h-[12.8125rem] w-[10.25rem]'
-            }`}
+            className={`cursor-pointer rounded-sm object-cover object-center ${IMAGE_CLASS}`}
           />
-          <button
-            className={`flex cursor-pointer flex-col items-center justify-center gap-4 rounded-sm bg-[#F8F8F8] ${
-              cardType === 'BUSINESS_CARD' ? 'h-[8rem] w-[8rem]' : 'h-[12.8125rem] w-[10.25rem]'
-            }`}
-          >
+          <button className={COMMON_BUTTON_CLASSES}>
             <p className="text-[1rem]/[1rem] font-bold text-[#666]">이미지 재변환</p>
             <Plus />
           </button>
-          <button
-            className={`flex cursor-pointer flex-col items-center justify-center gap-4 rounded-sm bg-[#F8F8F8] ${
-              cardType === 'BUSINESS_CARD' ? 'h-[8rem] w-[8rem]' : 'h-[12.8125rem] w-[10.25rem]'
-            }`}
-          >
+          <button className={COMMON_BUTTON_CLASSES}>
             <p className="text-[1rem]/[1rem] font-bold text-[#666]">
               이미지
               <br />
