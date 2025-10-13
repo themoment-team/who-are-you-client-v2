005186582884961f7814c201904503a140d94d10
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-13 20:12:17 +0900

    style: CameraGuide  추가

---
+++ src/assets/CameraGuide.tsx
@@ -0,0 +1,28 @@
+const CameraGuide = () => (
+  <svg
+    width="704"
+    height="396"
+    viewBox="0 0 704 396"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      opacity="0.5"
+      d="M692 0C698.627 0 704 5.37258 704 12V384C704 390.627 698.627 396 692 396H12C5.37258 396 1.93283e-07 390.627 0 384V12C0 5.37258 5.37258 1.93277e-07 12 0H692ZM211 48C206.029 48 202 52.0294 202 57V339C202 343.971 206.029 348 211 348H493C497.971 348 502 343.971 502 339V57C502 52.0294 497.971 48 493 48H211Z"
+      fill="#222222"
+      style={{ fill: '#222222', fillOpacity: 1 }}
+    />
+    <rect
+      x="203"
+      y="49"
+      width="298"
+      height="298"
+      rx="8"
+      stroke="#D9D9D9"
+      style={{ stroke: '#D9D9D9', strokeOpacity: 1 }}
+      strokeWidth={2}
+    />
+  </svg>
+);
+
+export default CameraGuide;
