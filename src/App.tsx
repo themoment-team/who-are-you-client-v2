3a0fbd63f8fdd18bea7c355c33b82a1ffd0a69d4
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-15 00:45:47 +0900

    style: CameraPage 컴포넌트 포맷팅

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
     <Provider>
       <div className="flex h-screen items-center justify-center bg-[#f8f8f8]">
         {step === STEP.START && <StartPage setStep={setStep} setCardType={setCardType} />}
-        {step === STEP.CAMERA && <CameraPage setStep={setStep} setImageUrl={setImageUrl}/>}
+        {step === STEP.CAMERA && <CameraPage setStep={setStep} setImageUrl={setImageUrl} />}
         {step === STEP.AI_CONVERSION && <AiConversionPage setStep={setStep} />}
         {step === STEP.INFO_INPUT && <InfoInputPage setStep={setStep} />}
         {step === STEP.THEME_SELECT && <ThemeSelectPage setStep={setStep} cardType={cardType} />}
