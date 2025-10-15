162ddf5838fd7721a065b51fc5dcb8f2e93a386f
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-15 09:28:43 +0900

    feat: userInfo 상태 추가 및 InfoInputPage에 전달

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,21 @@ import {
   StartPage,
   ThemeSelectPage,
 } from './pageContainer';
-import { type CardType, STEP, type Step } from './types';
+import { type CardType, STEP, type Step, type userInfoFormType } from './types';
 
 const App = () => {
   const [step, setStep] = useState<Step>(STEP.START);
   const [cardType, setCardType] = useState<CardType | undefined>(undefined);
-
+  const [userInfo, setUserInfo] = useState<userInfoFormType | null>(null);
   return (
     <Provider>
       <div className="flex h-screen items-center justify-center bg-[#f8f8f8]">
         {step === STEP.START && <StartPage setStep={setStep} setCardType={setCardType} />}
         {step === STEP.CAMERA && <CameraPage setStep={setStep} />}
         {step === STEP.AI_CONVERSION && <AiConversionPage setStep={setStep} />}
-        {step === STEP.INFO_INPUT && <InfoInputPage setStep={setStep} />}
+        {step === STEP.INFO_INPUT && (
+          <InfoInputPage userInfo={userInfo} setUserInfo={setUserInfo} setStep={setStep} />
+        )}
         {step === STEP.THEME_SELECT && <ThemeSelectPage setStep={setStep} cardType={cardType} />}
       </div>
     </Provider>
