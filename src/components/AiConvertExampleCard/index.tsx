63e03c35a01473992584681958bf6ce018ad87c8
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-10-15 09:33:48 +0900

    feat: 변환 예시 Card 컴포넌트

---
+++ src/components/AiConvertExampleCard/index.tsx
@@ -0,0 +1,37 @@
+import type { SetStateAction } from 'react';
+
+import type { PromptType } from '../../types';
+
+interface AiConvertExampleCardProps {
+  title: PromptType;
+  img: string;
+  selectedPrompt: PromptType;
+  setSelectedPrompt: React.Dispatch<SetStateAction<PromptType>>;
+}
+
+const AiConvertExampleCard = ({
+  title,
+  img,
+  selectedPrompt,
+  setSelectedPrompt,
+}: AiConvertExampleCardProps) => {
+  const handleOnClick = () => {
+    setSelectedPrompt(title);
+  };
+
+  return (
+    <div
+      className={`flex w-32 flex-col items-center gap-4 rounded-xl border border-solid border-[#222] p-4 ${selectedPrompt === title && 'bg-black'}`}
+      onClick={handleOnClick}
+    >
+      <p
+        className={`text-center text-xl leading-[1.625rem] font-bold whitespace-nowrap text-[#222] ${selectedPrompt === title && 'text-white'}`}
+      >
+        {title}
+      </p>
+      <img src={img} className="h-24 w-24 rounded-xl" />
+    </div>
+  );
+};
+
+export default AiConvertExampleCard;
