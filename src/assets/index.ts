b6d9cc6fe0f699ceca2a37017dbe5cad90655576
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-15 14:03:57 +0900

    style: FourCutCameraGuide 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,3 +7,4 @@ export { default as LongBar } from './LongBar';
 export { default as Plus } from './Plus';
 export { default as Spin } from './Spin';
 export { default as BusinessCard } from './BusinessCardCameraGuide';
+export { default as FourCutCameraGuide } from './FourCutCameraGuide';
