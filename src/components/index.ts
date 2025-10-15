86f97c199b4e8661ded05d6bd864ddb1ebfafea5
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-15 09:32:11 +0900

    feat: InputFormItem 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './BusinessCard';
 export * from './FourCut';
 export { default as StepButton } from './StepButton';
 export { default as PhotoReselectModal } from './PhotoReselectModal';
+export { default as InputFormItem } from './InputFormItem';
