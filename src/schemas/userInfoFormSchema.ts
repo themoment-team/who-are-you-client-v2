d5dc93145c0bbe07816cbb05f63b7408f05025fe
효주 <164720957+h-0y28@users.noreply.github.com>
2025-10-15 10:13:26 +0900

    feat: userInfoFormSchema 전화번호 유효성 검사 및 메시지 수정

--- src/schemas/userInfoFormSchema.ts
+++ src/schemas/userInfoFormSchema.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod';
 
 export const userInfoFormSchema = z.object({
-  name: z.string().min(1, '한 글자 이상 입력해주세요'),
-  tel: z.string().regex(/^01[0-9]\d{7,8}$/, '-(하이픈) 없이 입력해주세요'),
-  email: z.string().email('유효한 이메일 주소를 입력해주세요').optional().or(z.literal('')),
-  major: z.string().optional().or(z.literal('')),
+  name: z.string().min(1, '이름을 입력해주세요'),
+  tel: z
+    .string()
+    .min(1, '전화번호를 입력해주세요')
+    .regex(/^\d{9,11}$/, {
+      message: '하이픈(-) 없이 숫자 9~11자리로 입력해주세요',
+    }),
+  email: z.string().email('유효한 이메일 주소를 입력해주세요').or(z.literal('')),
+  major: z.string().or(z.literal('')),
 });
