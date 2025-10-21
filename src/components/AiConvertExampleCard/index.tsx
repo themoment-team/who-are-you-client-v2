dbc6fd278702b26cf89e5fb4d49ff52ae9029ae5
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-10-21 20:20:15 +0900

    feat: AI 변환 예시 카드 애니메이션 추가

--- src/components/AiConvertExampleCard/index.tsx
+++ src/components/AiConvertExampleCard/index.tsx
@@ -18,16 +18,20 @@ const AiConvertExampleCard = ({
   };
 
   return (
-    <div
-      className={`flex w-32 cursor-pointer flex-col items-center gap-4 rounded-xl border border-solid border-[#222] p-4 ${selectedPrompt === title && 'bg-black'}`}
-      onClick={handleOnClick}
-    >
-      <p
-        className={`text-center text-xl leading-[1.625rem] font-bold whitespace-nowrap text-[#222] ${selectedPrompt === title && 'text-white'}`}
+    <div className="relative" onClick={handleOnClick}>
+      <div
+        className={`flex w-32 cursor-pointer flex-col items-center gap-4 rounded-xl border border-solid border-[#222] p-4 transition-all duration-700 ${selectedPrompt === title && 'bg-black'}`}
       >
-        {title}
-      </p>
-      <img src={img} className="h-24 w-24 rounded-xl" />
+        <p
+          className={`text-center text-xl leading-[1.625rem] font-bold whitespace-nowrap text-[#222] ${selectedPrompt === title && 'text-white'}`}
+        >
+          {title}
+        </p>
+        <img src={img} className="h-24 w-24 rounded-xl" />
+      </div>
+      {selectedPrompt !== title && selectedPrompt !== null && (
+        <div className="absolute inset-0 h-full w-32 rounded-xl bg-white/50 transition-opacity duration-500" />
+      )}
     </div>
   );
 };
