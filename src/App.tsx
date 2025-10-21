4b36a832996ba32f6ce907cff8b69a41b3784901
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-10-21 19:47:51 +0900

    feat: ai 연결 틀 제작

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,24 @@ import {
   StartPage,
   ThemeSelectPage,
 } from './pageContainer';
-import { type CardType, STEP, type Step } from './types';
+import { type CardType, type PromptType, STEP, type Step } from './types';
+import { postConvertImage } from './utils';
 
 const App = () => {
   const [step, setStep] = useState<Step>(STEP.START);
   const [cardType, setCardType] = useState<CardType | undefined>(undefined);
   const [imageUrl, setImageUrl] = useState<string>('');
 
-  const array = [''];
+  const [selectedPrompt, setSelectedPrompt] = useState<PromptType>(null);
+
+  const AiImageConvert = async (img: string) => {
+    const newImageUrl = await postConvertImage(img, selectedPrompt);
+    setImageUrl(newImageUrl);
+  };
+
+  const handleAiConvert = () => {
+    // imageUrl.map((x) => AiImageConvert(x));
+  };
 
   return (
     <Provider>
@@ -23,7 +33,13 @@ const App = () => {
         {step === STEP.START && <StartPage setStep={setStep} setCardType={setCardType} />}
         {step === STEP.CAMERA && <CameraPage setStep={setStep} setImageUrl={setImageUrl} />}
         {step === STEP.AI_CONVERSION && (
-          <AiConversionPage setStep={setStep} cardType={cardType} imgs={array} />
+          <AiConversionPage
+            setStep={setStep}
+            cardType={cardType}
+            selectedPrompt={selectedPrompt}
+            setSelectedPrompt={setSelectedPrompt}
+            handleAiConvert={handleAiConvert}
+          />
         )}
         {step === STEP.INFO_INPUT && <InfoInputPage setStep={setStep} />}
         {step === STEP.THEME_SELECT && <ThemeSelectPage setStep={setStep} cardType={cardType} />}
