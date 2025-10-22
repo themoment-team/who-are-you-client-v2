33eb4b6ae48c5bad0e8d21efa8369d32a1b33de2
junjuny0227 <junjuny0227@gmail.com>
2025-10-22 15:24:31 +0900

    refactor: 네컷사진 테마 수정 및 불필요한 테마 삭제

--- src/components/FourCut/Theme1/index.tsx
+++ src/components/FourCut/Theme1/index.tsx
@@ -3,33 +3,33 @@ import type { FourCutProps } from '../../../types';
 
 const FourCutTheme1 = ({ imageSrcs, onImageClick }: FourCutProps) => {
   return (
-    <div className="flex h-[32.5rem] w-[21.5rem] flex-col gap-7 bg-white px-[1rem] pt-[1.5rem] pb-[1.6875rem] shadow-[0_2px_20px_0_rgba(177,177,177,0.25)] print:shadow-none print:outline print:outline-[#CFCFCF]">
-      <div className="flex flex-col gap-2">
-        <div className="flex gap-2">
+    <div className="flex h-[14.8cm] w-[10cm] flex-col gap-6 bg-white px-[1.125rem] py-[1.5rem] shadow-[0_2px_20px_0_rgba(177,177,177,0.25)] print:shadow-none print:outline print:outline-[#CFCFCF]">
+      <div className="flex flex-col items-center gap-[.9375rem]">
+        <div className="flex gap-[.9375rem]">
           <img
             src={imageSrcs[0]}
             alt="image 1"
-            className="h-[11.875rem] w-[9.5rem] cursor-pointer object-cover object-center"
+            className="h-[12.625rem] w-[10.125rem] cursor-pointer object-cover object-center"
             onClick={onImageClick}
           />
           <img
             src={imageSrcs[1]}
             alt="image 2"
-            className="h-[11.875rem] w-[9.5rem] cursor-pointer object-cover object-center"
+            className="h-[12.625rem] w-[10.125rem] cursor-pointer object-cover object-center"
             onClick={onImageClick}
           />
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-[.9375rem]">
           <img
             src={imageSrcs[2]}
             alt="image 3"
-            className="h-[11.875rem] w-[9.5rem] cursor-pointer object-cover object-center"
+            className="h-[12.625rem] w-[10.125rem] cursor-pointer object-cover object-center"
             onClick={onImageClick}
           />
           <img
             src={imageSrcs[3]}
             alt="image 4"
-            className="h-[11.875rem] w-[9.5rem] cursor-pointer object-cover object-center"
+            className="h-[12.625rem] w-[10.125rem] cursor-pointer object-cover object-center"
             onClick={onImageClick}
           />
         </div>
